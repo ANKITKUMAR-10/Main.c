882177f90b8b36d826a5ef467143bb17bf9816a6
ANKITKUMAR-10 <95173788+ANKITKUMAR-10@users.noreply.github.com>
2022-01-16 00:04:06 +0530

    Add files via upload

---
+++ main.c/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main()
+{
+   int a,b ;
+   printf("Enter number a\n");
+   scanf("%d" , &a);
+
+   printf("enter a number b\n");
+   scanf("%d" , &b);
+
+   printf("multipication of the number a*b %d" , a*b);
+} 
